33ce09f008b1f263167f4d29cfbfa65ab3750df4
evgenia-pospelova <72083483+evgenia-pospelova@users.noreply.github.com>
2021-05-17 22:57:41 +0300

    Add files via upload

---
+++ Laboratornaya2_3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <semaphore.h>
+#include <windows.h>
+#define HAVE_STRUCT_TIMESPEC
+#include <pthread.h>
+
+using namespace std;
+
+bool flag = true;
+sem_t sem;
+struct timespec ts;
+
+void* func1(void* args) {
+	while (flag) {
+		if ()
+		sem_timedwait(&sem,&ts);
+		for (int i = 0; i < 10; i++) {
+			if (!flag) {
+				break;
+			}
+			std::cout << "1";
+			std::cout.flush();
+			Sleep(1);
+		}
+		sem_post(&sem);
+		Sleep(1);
+
+		return NULL;
+	}
+}
+void* func2(void* args) {
+	while (flag) {
+
+		sem_timedwait(&sem,&ts);
+		for (int i = 0; i < 10; i++) {
+			if (!flag) {
+				break;
+			}
+			std::cout << "2";
+			std::cout.flush();
+			Sleep(1);
+		}
+		sem_post(&sem);
+		Sleep(1);
+
+		return NULL;
+	}
+}
+
+int main()
+{
+	sem_init(&sem, 0, 1);
+	int arg = 10;
+	pthread_t thread1, thread2;
+	pthread_create(&thread1, NULL, func1, &arg);
+	pthread_create(&thread2, NULL, func2, &arg);
+	flag = false;
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	sem_destroy(&sem);
+	return 0;
+}
